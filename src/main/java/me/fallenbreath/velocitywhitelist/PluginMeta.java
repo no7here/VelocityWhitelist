e1da2eb57c558f24034c6de46ea36efd584c28a7
no7here <102915075+no7here@users.noreply.github.com>
2026-07-23 03:06:00 +0100

    Update repository URL in PluginMeta.java

--- src/main/java/me/fallenbreath/velocitywhitelist/PluginMeta.java
+++ src/main/java/me/fallenbreath/velocitywhitelist/PluginMeta.java
@@ -5,5 +5,5 @@ public class PluginMeta
 	public static final String ID = "velocitywhitelist";
 	public static final String NAME = "VelocityWhitelistExtended";
 	public static final String VERSION = "0.4.0";
-	public static final String REPOSITORY_URL = "https://github.com/no7here/VelocityWhitelist";
+	public static final String REPOSITORY_URL = "https://github.com/no7here/VelocityWhitelistExtended";
 }
